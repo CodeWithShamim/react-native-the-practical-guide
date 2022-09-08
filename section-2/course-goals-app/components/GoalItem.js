af3e5f3152a0e3caf71593e498ca43b364462631
codewithshamim <shamimislamonline@gmail.com>
2022-09-08 12:31:27 +0600

    ripple effect added

--- section-2/course-goals-app/components/GoalItem.js
+++ section-2/course-goals-app/components/GoalItem.js
@@ -11,19 +11,21 @@ const GoalItem = ({ goals, handleDeleteGoalItem }) => {
       data={goals}
       renderItem={({ item }) => {
         return (
-          <Pressable>
-            <View style={styles.goalsContainer}>
-              <View style={{ flexDirection: "row" }}>
-                <Icon name="rocket" size={20} color="purple"></Icon>
-                <Text style={{ paddingLeft: 8 }}>{item.name}</Text>
-              </View>
-              <Icon2
-                onPress={handleDeleteGoalItem.bind(this, item.id)}
-                name="circle-with-cross"
-                size={20}
-                color="red"
-              ></Icon2>
+          <Pressable
+            android_ripple={{ color: "#ffffff" }}
+            // style={({ pressed }) => pressed && styles.pressedStyle}
+            style={styles.goalsContainer}
+          >
+            <View style={{ flexDirection: "row" }}>
+              <Icon name="rocket" size={20} color="purple"></Icon>
+              <Text style={{ paddingLeft: 8 }}>{item.name}</Text>
             </View>
+            <Icon2
+              onPress={handleDeleteGoalItem.bind(this, item.id)}
+              name="circle-with-cross"
+              size={20}
+              color="red"
+            ></Icon2>
           </Pressable>
         );
       }}
@@ -43,5 +45,8 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     padding: 10,
     marginBottom: 10,
+  },
+  pressedStyle: {
+    opacity: 0.5,
   },
 });
