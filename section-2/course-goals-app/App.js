00fd025e967e71d95fa4ce85c581421538c0bb89
codewithshamim <shamimislamonline@gmail.com>
2022-09-08 13:00:24 +0600

    Modal added

--- section-2/course-goals-app/App.js
+++ section-2/course-goals-app/App.js
@@ -1,17 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Button } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import GoalInput from "./components/GoalInput";
 import GoalItem from "./components/GoalItem";
 
 export default function App() {
   const [goals, setGoals] = useState([]);
+  const [modalIsVisible, setModalIsVisible] = useState(false);
 
   const handleDeleteGoalItem = (id) => {
     setGoals((currentGoals) => {
       return currentGoals.filter((goal) => goal.id !== id);
     });
+  };
+
+  const handleModalVisibility = () => {
+    setModalIsVisible(true);
   };
 
   return (
@@ -20,8 +25,19 @@ export default function App() {
       <View style={styles.appContainer}>
         <Icon name="home" size={48} color="#facc15"></Icon>
 
+        {/* --------modal visible button----------- */}
+        <Button
+          title="Add New Goal"
+          color="#dbae0d"
+          onPress={handleModalVisibility}
+        ></Button>
+
         {/* input box  */}
-        <GoalInput setGoals={setGoals}></GoalInput>
+        <GoalInput
+          setGoals={setGoals}
+          modalIsVisible={modalIsVisible}
+          setModalIsVisible={setModalIsVisible}
+        ></GoalInput>
 
         {/* course goal item  */}
         <GoalItem
--- section-2/course-goals-app/components/GoalInput.js
+++ section-2/course-goals-app/components/GoalInput.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Button, TextInput, View, StyleSheet } from "react-native";
+import { Button, TextInput, View, StyleSheet, Modal } from "react-native";
 
-const GoalInput = ({ setGoals }) => {
+const GoalInput = ({ setGoals, modalIsVisible, setModalIsVisible }) => {
   const [inputText, setInputText] = useState("");
 
   const getInputText = (txt) => {
@@ -14,21 +14,41 @@ const GoalInput = ({ setGoals }) => {
         ...goals,
         { id: Math.random().toString(), name: inputText },
       ]);
+      setModalIsVisible(false);
     }
     setInputText("");
   };
 
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        onChangeText={getInputText}
-        style={styles.inputText}
-        value={inputText}
-        placeholder="Enter your course goal!"
-        placeholderTextColor="#d1bebe"
-      ></TextInput>
-      <Button onPress={addInputText} title="Add Goal" color="#dbae0d"></Button>
-    </View>
+    <Modal
+      visible={modalIsVisible}
+      animationType="slide"
+      style={{ backgroundColor: "#ed0b53" }}
+    >
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={getInputText}
+          style={styles.inputText}
+          value={inputText}
+          placeholder="Enter your course goal!"
+          placeholderTextColor="#d1bebe"
+        ></TextInput>
+        {/* btn container  */}
+        <View style={styles.btnContainer}>
+          <Button
+            onPress={addInputText}
+            title="Add Goal"
+            color="#dbae0d"
+          ></Button>
+
+          <Button
+            onPress={() => setModalIsVisible(false)}
+            title="Cancel"
+            color="red"
+          ></Button>
+        </View>
+      </View>
+    </Modal>
   );
 };
 
@@ -36,11 +56,14 @@ export default GoalInput;
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flexDirection: "row",
-    width: "90%",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
     borderBottomColor: "#fff",
     paddingBottom: 20,
     borderBottomWidth: 2,
+    backgroundColor: "#ed0b53",
   },
   inputText: {
     borderColor: "#facc15",
@@ -52,9 +75,15 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     borderRadius: 4,
     marginRight: 10,
-    width: "70%",
+    width: "90%",
     placeholderColor: "blue",
     shadowColor: "#000000",
     elevation: 50,
+  },
+  btnContainer: {
+    flexDirection: "row",
+    width: "50%",
+    justifyContent: "space-between",
+    marginTop: 10,
   },
 });
