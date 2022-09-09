a558d085547babdde079edeb7e695d62bdb166fb
codewithshamim <shamimislamonline@gmail.com>
2022-09-09 21:31:32 +0600

    creating, using & displaying random numbers

---
+++ section-4/guess-game-app/components/game/NumberContainer.js
@@ -0,0 +1,28 @@
+import { View, Text, StyleSheet } from "react-native";
+import { secondaryColor, whiteColor } from "../../constants/colors";
+
+const NumberContainer = ({ children }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.numberText}>{children}</Text>
+    </View>
+  );
+};
+
+export default NumberContainer;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 60,
+    marginHorizontal: 30,
+  },
+  numberText: {
+    textAlign: "center",
+    fontSize: 26,
+    color: whiteColor,
+    fontWeight: "bold",
+    borderWidth: 3,
+    borderColor: secondaryColor,
+    padding: 12,
+  },
+});
--- section-4/guess-game-app/screens/GameScreen.js
+++ section-4/guess-game-app/screens/GameScreen.js
@@ -1,10 +1,35 @@
-import { View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/PrimaryButton";
 import Title from "../components/Title";
+import { secondaryColor, whiteColor } from "../constants/colors";
 
-const GameScreen = () => {
+// -----------generate random number-------------
+function generateRandomNumber(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomNumber(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+const GameScreen = ({ userNumber }) => {
+  const initialGuess = generateRandomNumber(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
   return (
     <View style={styles.container}>
       <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Text style={styles.headTitle}>Higher or lower?</Text>
+
+      <View style={styles.btnContainer}>
+        <PrimaryButton bgColor={secondaryColor}>+</PrimaryButton>
+        <PrimaryButton bgColor={secondaryColor}>-</PrimaryButton>
+      </View>
     </View>
   );
 };
@@ -13,9 +38,24 @@ export default GameScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignContent: "center",
     marginTop: 80,
     marginHorizontal: 15,
+  },
+  headTitle: {
+    fontSize: 20,
+    marginVertical: 30,
+    fontWeight: "bold",
+    color: whiteColor,
+    letterSpacing: 2,
+    textTransform: "uppercase",
+    textAlign: "center",
+  },
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  plusBtn: {
+    fontSize: 20,
   },
 });
