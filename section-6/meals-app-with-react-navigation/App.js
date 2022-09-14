a453190f5cffe497ab3da02a5da4c64dad56dde0
codewithshamim <shamimislamonline@gmail.com>
2022-09-14 23:21:58 +0600

    setting header title dynamically

--- section-6/meals-app-with-react-navigation/App.js
+++ section-6/meals-app-with-react-navigation/App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from "expo-status-bar";
-import { View, StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -34,15 +33,15 @@ export default function App() {
           <Stack.Screen
             name="MealsOverview"
             component={MealsOverviewScreen}
+            // options={({ route, navigation }) => {
+            //   const id = route.params.id;
+            //   return {
+            //     title: id,
+            //   };
+            // }}
           ></Stack.Screen>
         </Stack.Navigator>
       </NavigationContainer>
     </>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-// });
--- section-6/meals-app-with-react-navigation/screens/MealsOverviewScreen.js
+++ section-6/meals-app-with-react-navigation/screens/MealsOverviewScreen.js
@@ -1,10 +1,10 @@
 import { View, FlatList, StyleSheet } from "react-native";
-import React from "react";
-import { MEALS } from "../data/dummy-data";
+import React, { useLayoutEffect } from "react";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
 // import { useRoute } from "@react-navigation/native";
 
-const MealsOverviewScreen = ({ route }) => {
+const MealsOverviewScreen = ({ route, navigation }) => {
   const meals = MEALS;
   const id = route.params.id;
 
@@ -15,6 +15,11 @@ const MealsOverviewScreen = ({ route }) => {
   const renderMealItem = ({ item }) => {
     return <MealItem item={item}></MealItem>;
   };
+
+  useLayoutEffect(() => {
+    const categoryName = CATEGORIES.find((category) => category.id === id);
+    navigation.setOptions({ title: categoryName.name });
+  }, [id, navigation]);
 
   return (
     <View style={styles.mealsContainer}>
